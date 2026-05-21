d17b8456674bc67b2c12eb567340b570616133a2
JuNaYeD <junayedpsrl@gmail.com>
2026-05-21 22:25:40 +0600

    Add inline comments to base Model class

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,28 +1,39 @@
 <?php
+
 namespace App\Models;
 
 use App\Core\Database;
 
+
+
 /**
  * Base Model
+ * Provides shared database access and table resolution for all models.
  */
-abstract class Model
+
+
+
+abstract class model
 {
     protected static string $table;
     protected Database $db;
 
-    public function __construct()
-    {
+    public function __construct(){
+
+        // Initialize database connection on model instantiation
+
         $this->db = Database::getInstance();
     }
 
-    public static function getTable(): string
-    {
+    public static function getTable(): string{
+
         return static::$table;
     }
 
     public static function db(): Database
     {
-        return Database::getInstance();
+        // Returns singleton database instance for static access
+
+         return Database::getInstance();
     }
 }
